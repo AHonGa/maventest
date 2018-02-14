348379e7b781c84332d26ffeaf0653a39ea22fde
Jinwoo Andrew Hong <ahong@mmm.com>
2018-02-14 18:01:40 -0500

    Add maven test project

---
+++ src/main/java/mmm/his/cdp/App.java
@@ -0,0 +1,13 @@
+package mmm.his.cdp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World From Maven!" );
+    }
+}
